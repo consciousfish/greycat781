667fd043dc87337cc03aa3b707d4099ecc7095f2
consciousfish <dertktog@gmail.com>
2026-05-15 01:06:24 +0300

    Delete greycat directory

--- greycat/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('sideMenu');
-    menu.classList.toggle('active');
-    
-    // Опционально: можно добавить анимацию самих полосок, чтобы они превращались в крестик
-}
